ea82cd928817a09d6ebbab3cefab86d5f7eda9a3
Ayushman <maanayush1002@gmail.com>
2022-12-31 20:21:21 +0530

    Add files via upload

---
+++ Leetcode/lengthOfLastWord.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count=0;
+        bool flag=false;
+        for(int i=s.size()-1; i>=0; i--)
+        {
+            if(s[i]==' ' && flag==true)
+            {
+                break;
+            }
+            else if(s[i]!=' ')
+            {
+                flag= true;
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
+int main(){
+    string s="I am Ayushman and I am the Best";
+    Solution sol;
+    int size=sol.lengthOfLastWord(s);
+
+    cout<<"Length of last word: "<<size<<endl;
+
+return 0;
+}
