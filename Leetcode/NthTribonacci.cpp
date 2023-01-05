677dab4c6270d68608172756892780b8b7651dfa
Ayushman <maanayush1002@gmail.com>
2023-01-05 15:42:40 +0530

    N-th Tribonacci Number leetcode solution

---
+++ Leetcode/NthTribonacci.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Solution
+{
+    public:
+        int getTrib(int n)
+        {
+            if(n==0)
+            {
+                return 0;
+            }
+            if(n<3)
+            {
+                return 1;
+            }
+            int x=0, y=1, z=1, ans;
+
+            for (int i = 3; i <=n; i++)
+            {
+                /* code */
+                ans=x+y+z;
+                x=y;
+                y=z;
+                z=ans;
+            }
+            return ans;
+        }
+};
+int main(){
+    int n;
+    cout<<"Enter the value of N:"<<endl;
+    cin>>n;
+
+    Solution sol;
+    int ans= sol.getTrib(n);
+
+    cout<<"The Nth tribonacci number is  :"<<ans<<endl;
+return 0;
+}
