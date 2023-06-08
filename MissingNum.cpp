c1da566f3db735f662120b47df72afd35225eddd
Ayushman <maanayush1002@gmail.com>
2023-06-08 23:19:03 +0530

    Create MissingNum.cpp

---
+++ MissingNum.cpp
@@ -0,0 +1,22 @@
+//GFG Missing number in array
+ int missingNumber(vector<int>& array, int n) {
+        //your code here
+        //using sum of numbers
+        int sumArr=0;
+        
+        for(int i=0; i<array.size();i++)
+        {
+            sumArr+=array[i];
+        }
+        int sumOfN= (n*(n+1))/2;
+        return sumOfN - sumArr;
+    }
+
+
+int main(){
+  vector<int>arr={1,2,3,5};
+  int n=5;
+  int ans = missingNumber(arr, n);
+  cout<<ans<<endl;
+  return 0;
+}
