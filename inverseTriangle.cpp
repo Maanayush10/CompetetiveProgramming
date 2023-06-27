f2efce0edc2b61b2f311d50cac0b38e558538cb5
Ayushman <maanayush1002@gmail.com>
2023-06-27 10:59:03 +0530

    Create inverseTriangle.cpp

---
+++ inverseTriangle.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int count= 5;
+    for (int i = 0; i < 5; i++)
+    {
+        /* code */
+        for(int k =count; k>=0; k--)
+        {
+            cout<<" ";
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            /* code */
+            cout<<"*";
+        }
+        count--;
+        cout<<endl;
+        
+    }
+    
+return 0;
+}
