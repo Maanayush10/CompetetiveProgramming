67a6ae0b934a8626526213f51807c105e57cf785
Ayushman <maanayush1002@gmail.com>
2023-07-07 13:53:08 +0530

    Update NTribonacci.c

--- HackerRank/NTribonacci.c
+++ HackerRank/NTribonacci.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 int tribonacci(int n){
-	if(n==0)
+    if(n==0)
     {
         return 0;
     }
+	
     if(n<3)
     {
         return 1;
     }
     int x=0, y=1, z=1, result;
+	
     for(int i=3; i<=n; i++)
     {
         result= x+y+z;
@@ -16,6 +18,7 @@ int tribonacci(int n){
         y=z;
         z=result;
     }
+	
     return result;
 }
 
