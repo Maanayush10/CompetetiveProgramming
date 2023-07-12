978c2745a22598b2dd42127f13c365825edf921b
Ayushman <maanayush1002@gmail.com>
2023-07-12 13:07:07 +0530

    Update 3sum.cpp

--- Leetcode/3sum.cpp
+++ Leetcode/3sum.cpp
@@ -47,7 +47,39 @@ vector<vector<int>> threeSum(vector<int>& nums) {
         return result;
     }
 
-//optimal approach using 2 pointers
+//using 2 pointers
+vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>>ans;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1, k=nums.size()-1;
+            vector<int>temp;
+
+            while(j<k)
+            {
+                if(nums[i]+ nums[j]+nums[k]<0)
+                {
+                    j++;
+                }
+            else if(nums[i]+ nums[j]+nums[k]>0)
+                {
+                    k--;
+                }
+            else
+                {
+                   vector<int>temp={nums[i], nums[j], nums[k]}; 
+                   ans.push_back(temp);
+                   j++;
+                   k--;
+                   while(j<k && nums[j]==nums[j-1])j++;
+                   while(j<k && nums[k]==nums[k+1])k--;
+                }
+            }
+        }
+        return ans;
+    }
 
 
 int main()
