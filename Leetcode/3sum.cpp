8bd54ac9f7031ae0bf329e0b982c94e743fb76f5
Ayushman <maanayush1002@gmail.com>
2023-07-12 12:43:00 +0530

    Update 3sum.cpp

--- Leetcode/3sum.cpp
+++ Leetcode/3sum.cpp
@@ -25,6 +25,30 @@ vector<vector<int>> threeSum(vector<int>& nums) {
         return result;
     }
 
+//better approach
+vector<vector<int>> threeSum(vector<int>& nums) {
+        set<vector<int>>st;
+        for(int i=0; i<nums.size(); i++)
+        {
+            set<int> hashset;
+            for(int j=i+1; j<nums.size(); j++)
+            {
+             int third = -(nums[i]+nums[j]);
+                if(hashset.find(third)!= hashset.end())
+                {
+                    vector<int> temp={nums[i], nums[j], third};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+                hashset.insert(nums[j]);  
+            }
+        }
+        vector<vector<int>> result(st.begin(), st.end());
+        return result;
+    }
+
+//optimal approach using 2 pointers
+
 
 int main()
 {
