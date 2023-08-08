3c745e0371015959de90af139c9e4d325e7d53a2
Ayushman <maanayush1002@gmail.com>
2023-08-08 16:00:02 +0530

    Create maxAreaOfwater.cpp

---
+++ Leetcode/maxAreaOfwater.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+//brute force o(N^2)
+int small(int num1,  int num2)
+    {
+        return num1<num2 ? num1 : num2;
+    }
+    int maxArea(vector<int>& height) {
+        vector<int> maxArr;
+
+        for(int i=0; i<height.size(); i++)
+        {
+            int max=0;
+            for(int j=i+1; j<height.size(); j++)
+            {
+                int smallNum= small(height[i], height[j]);
+                int prod= smallNum *(j-i);
+                if(max < prod)
+                {
+                    max= prod;
+                }
+            }
+            maxArr.push_back(max);
+        }
+        int result=maxArr[0];
+        for(int i=0; i<maxArr.size(); i++)
+        {
+            if(maxArr[i]> result)
+            {
+                result= maxArr[i];
+            }
+        }
+        return result;
+    }
+
+
+
+int main()
+{
+  //main logic here
+  return 0;
+}
