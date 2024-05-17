7d51011027238d374f965f96c27c1e0c06415c52
Ayushman <maanayush1002@gmail.com>
2024-05-17 15:53:54 +0530

    Create inorderSuccessorBST.cpp

---
+++ GFG/inorderSuccessorBST.cpp
@@ -0,0 +1,96 @@
+/*
+Inorder Successor in BST
+
+Given a BST, and a reference to a Node x in the BST. Find the Inorder Successor of the given node in the BST.
+ 
+
+Example 1:
+
+Input:
+      2
+    /   \
+   1     3
+K(data of x) = 2
+Output: 3 
+Explanation: 
+Inorder traversal : 1 2 3 
+Hence, inorder successor of 2 is 3.
+
+Example 2:
+
+Input:
+             20
+            /   \
+           8     22
+          / \
+         4   12
+            /  \
+           10   14
+K(data of x) = 8
+Output: 10
+Explanation:
+Inorder traversal: 4 8 10 12 14 20 22
+Hence, successor of 8 is 10.
+ 
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function inOrderSuccessor(). This function takes the root node and the reference node as argument and returns the node that is inOrder successor of the reference node. If there is no successor, return null value.
+
+
+Expected Time Complexity: O(Height of the BST).
+Expected Auxiliary Space: O(1).
+
+
+Constraints:
+1 <= N <= 105, where N is number of nodes
+
+*/
+
+
+// } Driver Code Ends
+/*The structure of Node
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution{
+  public:
+    // returns the inorder successor of the Node x in BST (rooted at 'root')
+    void inorder(Node * root, vector<int> &arr)
+    {
+        if(root==nullptr)
+        {
+            return;
+        }
+        inorder(root->left, arr);
+        arr.push_back(root->data);
+        inorder(root->right, arr);
+    }
+    Node * inOrderSuccessor(Node *root, Node *x)
+    {
+        //Your code here
+        vector<int> result;
+        inorder(root, result);
+        int index= 1;
+        for(int i=0; i<result.size(); i++)
+        {
+            if(result[i]== x->data)
+            {
+                index= i+1;
+                break;
+            }
+        }
+        if(index==result.size()){return nullptr;}
+        return new Node(result[index]);
+        
+    }
+};
