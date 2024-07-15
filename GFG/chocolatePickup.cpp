654d1ef4fcb13ba9f6090953e2e8196576169f89
Ayushman <maanayush1002@gmail.com>
2024-07-15 13:40:06 +0530

    Update chocolatePickup.cpp

--- GFG/chocolatePickup.cpp
+++ GFG/chocolatePickup.cpp
@@ -67,4 +67,44 @@ public:
         vector<vector<vector<int>>>dp(n, vector<vector<int>>(m, vector<int>(m, -1)));
         return f(0, 0, m - 1, n, m, grid, dp);
     }
+//tabulation approach (shoutout to striver)
+    int solve(int n, int m, vector<vector<int>>& grid) {
+        //tabulation
+        vector<vector<vector<int>>>dp(n, vector<vector<int>>(m, vector<int>(m, -1)));
+        for(int j1=0; j1<m; j1++)
+        {
+            for(int j2=0; j2<m; j2++)
+            {
+                if(j1==j2) dp[n-1][j1][j2]= grid[n-1][j1];
+                else dp[n-1][j1][j2]= grid[n-1][j1] + grid[n-1][j2];
+            }
+        }
+        
+        for(int i=n-2; i>=0; i--)
+        {
+            for(int j1=0; j1<m; j1++)
+            {
+                for(int j2=0; j2<m; j2++)
+                {
+                    int maxChoco = 0;
+                    for (int newCol1 = j1 - 1; newCol1 <= j1 + 1; newCol1++) {
+                        for (int newCol2 = j2 - 1; newCol2 <= j2 + 1; newCol2++) {
+                            // Changed '> 0' to '>= 0' to correctly handle boundary conditions
+                            if (newCol1 >= 0 && newCol1 < m && newCol2 >= 0 && newCol2 < m) {
+                                int currentChoco;
+                                if (j1 == j2) {
+                                    currentChoco = grid[i][j1] + dp[i + 1][newCol1][newCol2];
+                                } else {
+                                    currentChoco = grid[i][j1] + grid[i][j2] + dp[i + 1][newCol1][newCol2];
+                                }
+                                maxChoco = max(maxChoco, currentChoco);
+                            }
+                        }
+                    }
+                    dp[i][j1][j2]= maxChoco;
+                }
+            }
+        }
+        return dp[0][0][m-1];
+    }
 };
